0163fb2452cad6b7208dc6aa5a71fa0c61837f17
Venkatasai S <88194548+VenkatsaaiS@users.noreply.github.com>
2021-09-04 10:36:20 +0530

    Add files via upload

---
+++ Selenium_Project/src/test/java/com/mypackage/se/Handling_iframe.java
@@ -0,0 +1,29 @@
+package com.mypackage.se;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Handling_iframe {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://demoqa.com/frames");
+
+        List<WebElement> iframeElements = driver.findElements(By.id("frame1"));
+        List<WebElement> iframeElements1 = driver.findElements(By.id("sampleHeading"));
+
+        System.out.println("Total number of iframes are " + iframeElements.size());
+        System.out.println("Total number of iframes are " + iframeElements1.size());
+
+        driver.close();
+
+    }
+}
